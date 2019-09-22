1a8378cbded4309a58c931b75cb7849ec8f3641b
xiaoguoping <xiaoguoping@meituan.com>
2019-09-22 16:27:54 +0800

    update dsbrige

--- app/src/main/java/com/xiaoguoping/jsbrige_demo/MainActivity.java
+++ app/src/main/java/com/xiaoguoping/jsbrige_demo/MainActivity.java
@@ -7,25 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+
+import wendu.dsbridge.CompletionHandler;
+import wendu.dsbridge.DWebView;
+import wendu.dsbridge.OnReturnValue;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainActivity self = this;
-    private WebView webview;
+    private DWebView webview;
     private Button refreshBtn;
     private Button showBtn;
-    private Button showBtn2;
     private TextView inputText;
-    private NativeSDK nativeSDK = new NativeSDK(this);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         webview= findViewById(R.id.webView);
         refreshBtn = findViewById(R.id.refreshBtn);
         showBtn = findViewById(R.id.showBtn);
-        showBtn2 = findViewById(R.id.showBtn2);
         inputText = findViewById(R.id.inputText);
 
 
@@ -44,24 +40,10 @@ public class MainActivity extends AppCompatActivity {
         // 打开webview执行js代码的权限
         webview.getSettings().setJavaScriptEnabled(true);
 
-        // 1. 拦截url方式
-        /* webview.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                if(!message.startsWith("jsbridge://")){
-                    return super.onJsAlert(view, url, message, result);
-                }
-                String text = message.substring(message.indexOf("=") + 1);
-                self.showNativeDialog(text);
 
-                result.confirm();
-                return true;
-            }
-        });*/
-
-        // 2. 注入jsApi的方式
+        // DSBridge 注入jsApi
         webview.setWebChromeClient(new WebChromeClient());
-        webview.addJavascriptInterface(new NativeBridge(this), "NativeBridge");
+        webview.addJavascriptObject(new JSApi(this), null);
 
         // 刷新页面
         refreshBtn.setOnClickListener(new View.OnClickListener() {
@@ -71,125 +53,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // 显示web弹窗
         showBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputValue = inputText.getText().toString().trim();
-                self.showWebDialog(inputValue);
+                webview.callHandler("getWebEditValue", null, new OnReturnValue<String>() {
 
-            }
-        });
-
-        // 获取web输入框的值
-        showBtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nativeSDK.getWebEditTextValue(new CallBack() {
                     @Override
-                    public void invoke(String value) {
-                        new AlertDialog.Builder(self)
-                                .setMessage("Web 输入值:" + value)
-                                .create()
-                                .show();
+                    public void onValue(String retValue) {
+                         new AlertDialog.Builder(self)
+                                 .setMessage("web输入值：" + retValue)
+                                 .create()
+                                 .show();
                     }
                 });
-
             }
         });
-
-    }
-    private void showWebDialog (String text){
-        String jsCode = String.format("window.showWebDialog('%s')", text);
-        webview.evaluateJavascript(jsCode, null);
     }
 
     /**
-     * native展示弹窗
-     * @param text
+     * 统一的JSApi管理
      */
-    private void showNativeDialog(String text){
-        new AlertDialog.Builder(this)
-                .setMessage(text)
-                .create()
-                .show();
-    }
-
-    // Native暴露给JS的桥
-    class NativeBridge {
+    class JSApi {
         private Context ctx;
-        NativeBridge (Context ctx){
-            this.ctx = ctx;
-        }
-
-        @JavascriptInterface
-        public void showNativeDialog(String text) {
-            new AlertDialog.Builder(ctx)
-                    .setMessage(text)
-                    .create()
-                    .show();
-        }
-
-        @JavascriptInterface
-        public void getNativeEditTextValue(int callbackId){
-            final MainActivity mainActivity = (MainActivity) ctx;
-            String value = mainActivity.inputText.getText().toString();
-            final String jsCode = String.format("window.JSSDK.receiveMessage(%s, '%s')", callbackId, value);
-            mainActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mainActivity.webview.evaluateJavascript(jsCode, null);
-                }
-            });
-
-        }
-
-        @JavascriptInterface
-        public void recieveMessage(int callbackId, String value){
-            ((MainActivity) ctx).nativeSDK.recieveMessage(callbackId, value);
-        }
-    }
-
-
-    // callback回调的接口
-    interface CallBack{
-        void invoke(String value);
-    }
-
-    // NativeSDK
-    class NativeSDK {
-        private Context ctx;
-        private int callbackId = 0;
-        private Map<Integer, CallBack> callBackMap = new HashMap();
-        NativeSDK(Context ctx){
+        public JSApi(Context ctx){
             this.ctx = ctx;
         }
 
         /**
-         * 调用js 代码获取web数据
-         * @param callback
+         * 暴露给js调用的api,
+         * @param params 参数
+         * @param handler 回调方法
          */
-        void getWebEditTextValue(CallBack callback){
-            callBackMap.put(callbackId, callback);
-            final String jsCode = String.format("window.JSSDK.getWebEditValue(%s)", callbackId);
-            ((MainActivity)ctx).runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    ((MainActivity) ctx).webview.evaluateJavascript(jsCode, null);
-                }
-            });
-            callbackId++;
-        }
-
-        /**
-         * 接收js回调的处理
-         * @param callbackId
-         * @param value
-         */
-        void recieveMessage(int callbackId, String value){
-            if(callBackMap.containsKey(callbackId)){
-                callBackMap.get(callbackId).invoke(value);
-            }
+        @JavascriptInterface
+        public void getNativeEditValue(Object params, CompletionHandler<String> handler){
+            String value = ((MainActivity)ctx).inputText.getText().toString();
+            handler.complete(value);
         }
     }
 
